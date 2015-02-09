0e732ec9c0e781e9206c70b887ae6cc6f5638853
Miguel Grinberg <miguelgrinberg50@gmail.com>
2015-02-08 23:59:17 -0800

    add totp two factor authentication

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
-from flask import Flask, render_template, redirect, url_for, flash
+import os
+import base64
+from io import StringIO
+from flask import Flask, render_template, redirect, url_for, flash, session, \
+    abort
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.login import LoginManager, UserMixin, login_user, logout_user, \
@@ -7,7 +11,8 @@ from flask.ext.bootstrap import Bootstrap
 from flask.ext.wtf import Form
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import Required, Length, EqualTo
-
+import onetimepass
+import pyqrcode
 
 # create application instance
 app = Flask(__name__)
@@ -25,6 +30,13 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True)
     password_hash = db.Column(db.String(128))
+    otp_secret = db.Column(db.String(16))
+
+    def __init__(self, **kwargs):
+        super(User, self).__init__(**kwargs)
+        if self.otp_secret is None:
+            # generate a random secret
+            self.otp_secret = base64.b32encode(os.urandom(10)).decode('utf-8')
 
     @property
     def password(self):
@@ -36,6 +48,13 @@ class User(UserMixin, db.Model):
 
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def get_totp_uri(self):
+        return 'otpauth://totp/2FA-Demo:{0}?secret={1}&issuer=2FA-Demo' \
+            .format(self.username, self.otp_secret)
+
+    def verify_totp(self, token):
+        return onetimepass.valid_totp(token, self.otp_secret)
 
 
 @lm.user_loader
@@ -57,6 +76,7 @@ class LoginForm(Form):
     """Login form."""
     username = StringField('Username', validators=[Required(), Length(1, 64)])
     password = PasswordField('Password', validators=[Required()])
+    token = StringField('Token', validators=[Required(), Length(6, 6)])
     submit = SubmitField('Login')
 
 
@@ -81,9 +101,48 @@ def register():
         user = User(username=form.username.data, password=form.password.data)
         db.session.add(user)
         db.session.commit()
-        flash('You are now registered!')
-        return redirect(url_for('login'))
+
+        # redirect to the two-factor auth page, passing username in session
+        session['username'] = user.username
+        return redirect(url_for('two_factor_setup'))
     return render_template('register.html', form=form)
+
+
+@app.route('/twofactor')
+def two_factor_setup():
+    if 'username' not in session:
+        return redirect(url_for('index'))
+    user = User.query.filter_by(username=session['username']).first()
+    if user is None:
+        return redirect(url_for('index'))
+    # since this page contains the sensitive qrcode, make sure the browser
+    # does not cache it
+    return render_template('two-factor-setup.html'), 200, {
+        'Cache-Control': 'no-cache, no-store, must-revalidate',
+        'Pragma': 'no-cache',
+        'Expires': '0'}
+
+
+@app.route('/qrcode')
+def qrcode():
+    if 'username' not in session:
+        abort(404)
+    user = User.query.filter_by(username=session['username']).first()
+    if user is None:
+        abort(404)
+
+    # for added security, remove username from session
+    del session['username']
+
+    # render qrcode for FreeTOTP
+    url = pyqrcode.create(user.get_totp_uri())
+    stream = StringIO()
+    url.svg(stream, scale=3)
+    return stream.getvalue().encode('utf-8'), 200, {
+        'Content-Type': 'image/svg+xml',
+        'Cache-Control': 'no-cache, no-store, must-revalidate',
+        'Pragma': 'no-cache',
+        'Expires': '0'}
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -95,9 +154,11 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
-        if user is None or not user.verify_password(form.password.data):
-            flash('Invalid username or password.')
+        if user is None or not user.verify_password(form.password.data) or \
+                not user.verify_totp(form.token.data):
+            flash('Invalid username, password or token.')
             return redirect(url_for('login'))
+
         # log user in
         login_user(user)
         flash('You are now logged in!')
