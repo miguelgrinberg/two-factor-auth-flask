3c67e44885335319a3ec215de9c2ddde28214bf3
Pouya1364 <pouya1364@gmail.com>
2020-08-08 18:56:03 +0800

    Update config.py
    change secret key

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
-SECRET_KEY = 'top-secret'
+SECRET_KEY = 'top-secret-KEY'
 SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///db.sqlite')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
